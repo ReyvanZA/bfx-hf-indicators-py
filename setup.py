121f698e4511750f15fade34767e3e1d64f2dfe9
ReyvanZA <rey@greenlight.co.za>
2019-10-08 21:00:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='bfxhfindicators', version='1.0.1', author='Cris Mihalache,Reynauld van Rooyen',
+setup(name='bfxhfindicators', version='1.0.2', author='Cris Mihalache,Reynauld van Rooyen',
       author_email='cris.m@bitfinex.com',
       url="https://github.com/ReyvanZA/bfx-hf-indicators-py", license='LICENSE',
       packages=['bfxhfindicators'], install_requires=[],
