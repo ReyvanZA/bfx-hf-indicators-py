55d9536047bcb7d0dd0debef2bbdd48e7d850de7
ReyvanZA <rey@greenlight.co.za>
2019-10-08 20:58:15 +0200

    Update setup.py
    Updated version and added co-author (Yes, I'm being cheeky, but no one is going to download my version anyway)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='bfxhfindicators', version='1.0.0', author='Cris Mihalache',
+setup(name='bfxhfindicators', version='1.0.1', author='Cris Mihalache,Reynauld van Rooyen',
       author_email='cris.m@bitfinex.com',
-      url="https://github.com/bitfinexcom/bfx-hf-strategy-hf", license='LICENSE',
+      url="https://github.com/ReyvanZA/bfx-hf-indicators-py", license='LICENSE',
       packages=['bfxhfindicators'], install_requires=[],
       description="Trading indicators library for the Bitfinex Honey Framework")
